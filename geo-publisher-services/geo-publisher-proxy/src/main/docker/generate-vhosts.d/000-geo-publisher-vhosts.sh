02070343179acb75e71dacc05fe9fcf51929d272
Erik Orbons <erik.orbons@idgis.nl>
2015-02-17 10:17:47 +0100

    Generate Apache vhost configuration based on environment.

---
+++ geo-publisher-services/geo-publisher-proxy/src/main/docker/generate-vhosts.d/000-geo-publisher-vhosts.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function writeVhostStart {
+	VHOST_FILE=/etc/apache2/sites-available/vhost.$1.conf
+	cat > $VHOST_FILE <<-EOT
+		<VirtualHost *:80>
+			ServerName $1
+			
+			DocumentRoot /var/www/overijssel.geo-hosting.nl
+			
+			<Location />
+				Options -Indexes
+			</Location>
+			
+			ProxyPreserveHost On
+EOT
+}
+
+function writeVhostEnd {
+	VHOST_FILE=/etc/apache2/sites-available/vhost.$1.conf
+	echo "</VirtualHost>" >> $VHOST_FILE
+	a2ensite vhost.$1.conf
+}
+
+function writeGeoserver {
+	cat >> $1 <<-EOT
+		ProxyPass /geoserver		ajp://geoserver:8009/geoserver
+		ProxyPassReverse /geoserver	ajp://geoserver:8009/geoserver
+		
+EOT
+}
+
+function writeWeb {
+	cat >> $1 <<-EOT
+		ProxyPass /metadata !
+		ProxyPass /icons !
+	
+		<Location /metadata>
+			Options +Indexes
+			Dav On
+		</Location>
+		
+		ProxyPass /				http://web:9000/
+		ProxyPassReverse /		http://web:9000/
+EOT
+}
+
+if [ "$PUBLISHER_GEOSERVER_DOMAIN" == "$PUBLISHER_WEB_DOMAIN" ]; then
+	# Write to the same vhost file:
+	writeVhostStart $PUBLISHER_WEB_DOMAIN
+	
+	writeGeoserver /etc/apache2/sites-available/vhost.$PUBLISHER_WEB_DOMAIN.conf
+	writeWeb /etc/apache2/sites-available/vhost.$PUBLISHER_WEB_DOMAIN.conf
+
+	writeVhostEnd $PUBLISHER_WEB_DOMAIN	
+else
+	# Write to separate vhost files:
+	writeVhostStart $PUBLISHER_WEB_DOMAIN
+	
+	writeWeb /etc/apache2/sites-available/vhost.$PUBLISHER_WEB_DOMAIN.conf
+
+	writeVhostEnd $PUBLISHER_WEB_DOMAIN	
+
+	writeVhostStart $PUBLISHER_GEOSERVER_DOMAIN	
+	
+	writeGeoserver /etc/apache2/sites-available/vhost.$PUBLISHER_GEOSERVER_DOMAIN.conf
+
+	writeVhostEnd $PUBLISHER_GEOSERVER_DOMAIN	
+fi
+
+cat /etc/apache2/sites-available/vhost.$PUBLISHER_WEB_DOMAIN.conf
+cat /etc/apache2/sites-available/vhost.$PUBLISHER_GEOSERVER_DOMAIN.conf
