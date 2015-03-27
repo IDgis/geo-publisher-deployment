03e2c1358f558f2ddee23215990db52ee2c47266
Erik Orbons <erikorbons@users.noreply.github.com>
2015-03-27 14:45:18 +0100

    Service configuration added to the registration WAR.

--- geo-publisher-services/geo-publisher-geoserver/src/main/docker/start.sh
+++ geo-publisher-services/geo-publisher-geoserver/src/main/docker/start.sh
@@ -6,4 +6,18 @@ if [ ! -e /var/lib/geo-publisher/geoserver/security ]; then
 	echo "$PUBLISHER_GEOSERVER_USER=$PUBLISHER_GEOSERVER_PASSWORD,ROLE_ADMINISTRATOR" > /var/lib/geo-publisher/geoserver/security/users.properties
 fi
 
+JAVA_OPTS="$JAVA_OPTS -Dservice.identification=$SERVICE_IDENTIFICATION"
+JAVA_OPTS="$JAVA_OPTS -DzooKeeper.hosts=$ZOOKEEPER_HOSTS"
+if [ "$ZOOKEEPER_NAMESPACE" != "" ]; then
+	JAVA_OPTS="$JAVA_OPTS -DzooKeeper.namespace=$ZOOKEEPER_NAMESPACE"
+fi
+JAVA_OPTS="$JAVA_OPTS -Dservice.domain=$SERVICE_DOMAIN"
+JAVA_OPTS="$JAVA_OPTS -Dservice.ajpPort=$SERVICE_AJP_PORT"
+JAVA_OPTS="$JAVA_OPTS -Dservice.httpPort=$SERVICE_HTTP_PORT"
+if [ "$SERVICE_IP" != "" ]; then
+	JAVA_OPTS="$JAVA_OPTS -Dservice.ip=$SERVICE_IP"
+fi
+JAVA_OPTS="$JAVA_OPTS -Dservice.path=$SERVICE_PATH"
+JAVA_OPTS="$JAVA_OPTS -Dservice.forceHttps=$SERVICE_FORCE_HTTPS"
+
 /usr/share/tomcat7/bin/catalina.sh run
