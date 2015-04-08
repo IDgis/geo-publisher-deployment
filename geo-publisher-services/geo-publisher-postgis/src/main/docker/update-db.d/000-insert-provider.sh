6d045f8f87783b756b7f70b54a2c4fee814d5ff9
Erik Orbons <erikorbons@users.noreply.github.com>
2015-04-08 13:14:08 +0200

    Added configuration for a vector and raster data source.

--- geo-publisher-services/geo-publisher-postgis/src/main/docker/update-db.d/000-insert-provider.sh
+++ geo-publisher-services/geo-publisher-postgis/src/main/docker/update-db.d/000-insert-provider.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-DSID=$(psql -d $PG_DATABASE -At -c "select identification from publisher.data_source where identification = '$PUBLISHER_DATA_SOURCE_ID';")
-if [[ "${DSID}" != "$PUBLISHER_DATA_SOURCE_ID" ]]; then
-	echo "Inserting default provider $PUBLISHER_DATA_SOURCE_NAME ($PUBLISHER_DATA_SOURCE_ID) ..."
-	psql -d $PG_DATABASE -At -c "insert into publisher.data_source(identification, name)values('$PUBLISHER_DATA_SOURCE_ID', '$PUBLISHER_DATA_SOURCE_NAME');"
-fi
+set -e
+
+function create_provider() {
+	DATA_SOURCE_ID="$1"
+	DATA_SOURCE_NAME="$2"	
+
+	DSID=$(psql -d $PG_DATABASE -At -c "select identification from publisher.data_source where identification = '$DATA_SOURCE_ID';")
+	if [[ "${DSID}" != "$DATA_SOURCE_ID" ]]; then
+		echo "Inserting default provider $DATA_SOURCE_NAME ($DATA_SOURCE_ID) ..."
+		psql -d $PG_DATABASE -At -c "insert into publisher.data_source(identification, name)values('$DATA_SOURCE_ID', '$DATA_SOURCE_NAME');"
+	fi
+}
+
+create_provider "$PUBLISHER_DATA_SOURCE_ID" "$PUBLISHER_DATA_SOURCE_NAME"
+create_provider "$PUBLISHER_RASTER_DATA_SOURCE_ID" "$PUBLISHER_RASTER_DATA_SOURCE_NAME"
