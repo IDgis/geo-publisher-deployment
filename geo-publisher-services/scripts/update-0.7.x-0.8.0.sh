398e4099e84f94bb35f5599c37361b3deebe1d4b
Erik Orbons <erikorbons@users.noreply.github.com>
2015-04-08 13:21:14 +0200

    Fixed SQL update statement.

--- geo-publisher-services/scripts/update-0.7.x-0.8.0.sh
+++ geo-publisher-services/scripts/update-0.7.x-0.8.0.sh
@@ -10,4 +10,4 @@ fi
 
 INSTANCE=$1
 
-docker exec gp-$INSTANCE-db psql -c "update publisher.data_source set identification = 'overijssel-gisbasip', name = 'Overijssel vectordata' where identification = 'overijssel';
+docker exec gp-$INSTANCE-db psql -c "update publisher.data_source set identification = 'overijssel-gisbasip', name = 'Overijssel vectordata' where identification = 'overijssel';" publisher
