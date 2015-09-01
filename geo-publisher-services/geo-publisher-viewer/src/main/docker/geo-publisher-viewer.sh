fd38e571db4646f74f9de0a5793bca92b17389b2
copierrj <reijer.copier@idgis.nl>
2015-09-01 12:22:19 +0200

    additional environment variables added

--- geo-publisher-services/geo-publisher-viewer/src/main/docker/geo-publisher-viewer.sh
+++ geo-publisher-services/geo-publisher-viewer/src/main/docker/geo-publisher-viewer.sh
@@ -6,6 +6,9 @@ if [ -e /opt/geopublisher-viewer/RUNNING_PID ]; then
 fi
 
 exec /opt/geopublisher-viewer/bin/geopublisher-viewer \
+	-DzooKeeper.hosts=$ZOOKEEPER_HOSTS \
+	-Dapplication.domain=$VIEWER_DOMAIN \
+        -Dplay.http.context=$VIEWER_CONTEXT \
         -Dviewer.environmenturl=$VIEWER_ENVIRONMENT_URL \
 	-Dviewer.username=$VIEWER_USERNAME \
 	-Dviewer.password=$VIEWER_PASSWORD
